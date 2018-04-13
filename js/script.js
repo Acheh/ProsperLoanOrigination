775c9450e04033b3dfb2881c21afaeed47db967a
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-04-13 16:25:00 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function draw(data) {
+  debugger;
+
+  const GRAPH_TITLE = "Prosper Marketplace Quarterly Loan Originations (2006-2013)";
+
+  // add title to the graph
+  d3.select('body')
+    .append('h2')
+      .text(GRAPH_TITLE);
+};
