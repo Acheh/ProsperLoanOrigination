c922aed6905958c7697f4dc9e7dee9a8f7831d2f
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-04-19 09:56:19 -0500

    add a function to process data into a stack and an array of data sum per period

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 function draw(data) {
 
   const GRAPH_TITLE = 'Prosper Marketplace Quarterly Loan Originations on the 36-month Loans';
+  const RISK_LEVELS = ['AA', 'A', 'B', 'C', 'D', 'E', 'HR'];
   
+  // process the data for barchart
+  processedData = processData();
+  let sumData = processedData[0]; // data dict of total loan amount in each quarter
+  let stackedData = processedData[1]; // stack of loan amount by risk by quarter
+
+  debugger;
+
   /*
     Draw the initial graph
   */
@@ -10,4 +18,39 @@ function draw(data) {
   d3.select('body')
     .append('h2')
       .text(GRAPH_TITLE);
+
+  /*
+    Functions
+  */
+
+  function processData() {
+    /*
+      Process data into a stack and a sum array
+      Returns:
+          An array of a stacked data and a sum array
+    */
+    let flatData = [];
+    let keys = [];
+    let dataSum = {};
+
+    for (period in data) {
+      let item = {};
+      item.period = period;
+      dataSum[period] = 0;
+      for (risk in data[period]) {
+        dataSum[period] += data[period][risk]['amount'];
+        item[risk] = data[period][risk]['amount'];
+      }
+      flatData.push(item);
+    }
+
+    RISK_LEVELS.reverse(); // reverse order to stack HR loan on the bottom of the chart
+    let stack = d3.stack()
+      .keys(RISK_LEVELS)
+      .order(d3.stackOrderNone)
+      .offset(d3.stackOffsetNone);
+    RISK_LEVELS.reverse(); // reverse back to previous order
+
+    return [dataSum, stack(flatData)];
+  };
 };
